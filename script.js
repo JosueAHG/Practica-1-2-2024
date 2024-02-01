55c51e20250b97cb9ac977752fb859becd9a7fa1
Merino <20230274@ricaldone.edu.sv>
2024-02-01 11:17:39 -0600

    Se agrego el contenido al js

---
+++ script.js
@@ -0,0 +1,6 @@
+function mostrarContenido() {
+    var texto1 = document.getElementById("textbox1").value;
+    var texto2 = document.getElementById("textbox2").value;
+
+    alert("Contenido de Texto 1: " + texto1 + "\nContenido de Texto 2: " + texto2);
+}
