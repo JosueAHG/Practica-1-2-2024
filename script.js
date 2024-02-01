6fc94c4aace3ea28875a410af5b0db4ab7c7144f
Merino <20230274@ricaldone.edu.sv>
2024-02-01 11:27:50 -0600

    Se agrego el contedio al css y se modificaron alugunas cosas al index y js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function mostrarContenido() {
-    var texto1 = document.getElementById("textbox1").value;
-    var texto2 = document.getElementById("textbox2").value;
+    var texto1 = document.getElementById("nom").value;
+    var texto2 = document.getElementById("ape").value;
 
-    alert("Contenido de Texto 1: " + texto1 + "\nContenido de Texto 2: " + texto2);
+    alert("nom " + texto1 + "\nape " + texto2);
 }
